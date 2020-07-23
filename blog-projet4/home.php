f63f7494801f4278bf15f84bee01b0e254baa834
Kevin Allioli <kevin@linit.io>
2020-07-23 16:45:12 +0200

    Add Article Class

--- blog-projet4/home.php
+++ blog-projet4/home.php
@@ -1,9 +1,9 @@
-
 <?php
 //On inclut le fichier dont on a besoin (ici à la racine de notre site)
 require 'Database.php';
+//Ne pas oublier d'ajouter le fichier Article.php
+require 'Article.php';
 ?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -12,15 +12,26 @@ require 'Database.php';
 </head>
 
 <body>
-    <div>
-        <h1>Mon blog</h1>
-        <p>En construction</p>
+<div>
+    <h1>Mon blog</h1>
+    <p>En construction</p>
+    <?php
+    $article = new Article();
+    $articles = $article->getArticles();
+    while($article = $articles->fetch())
+    {
+        ?>
+        <div>
+            <h2><a href="single.php?articleId=<?= htmlspecialchars($article->id);?>"><?= htmlspecialchars($article->title);?></a></h2>
+            <p><?= htmlspecialchars($article->content);?></p>
+            <p><?= htmlspecialchars($article->author);?></p>
+            <p>Créé le : <?= htmlspecialchars($article->createdAt);?></p>
+        </div>
+        <br>
         <?php
-    //On crée un nouvel objet $db, qui est une instance de la classe Database
-    $db = new Database();
-    //On fait appel à notre méthode getConnection()
-    $db->getConnection();
+    }
+    $articles->closeCursor();
     ?>
-    </div>
+</div>
 </body>
 </html>
