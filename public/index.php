f3e008b781a11be4a9dd5f36d13599f8cb94f484
Kevin Allioli <kevin@linit.io>
2020-08-04 10:16:46 +0200

    Add Frontend controller and Router

--- public/index.php
+++ public/index.php
@@ -1,22 +1,7 @@
 <?php
 
+require '../config/dev.php';
 require '../vendor/autoload.php';
 
-try{
-    if(isset($_GET['route']))
-    {
-        if($_GET['route'] === 'article'){
-            require '../templates/single.php';
-        }
-        else{
-            echo 'page inconnue';
-        }
-    }
-    else{
-        require '../templates/home.php';
-    }
-}
-catch (Exception $e)
-{
-    echo 'Erreur';
-}
+$router = new \App\config\Router();
+$router->run();
