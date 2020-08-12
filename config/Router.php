b2cc94a906abe2407f2672a3b58ec3273faff0df
Kevin Allioli <kevin@linit.io>
2020-08-12 12:04:48 +0200

    Add user managements

--- config/Router.php
+++ config/Router.php
@@ -48,6 +48,21 @@ class Router
                 elseif($route === 'deleteComment'){
                     $this->backController->deleteComment($this->request->getGet()->get('commentId'));
                 }
+                elseif($route === 'register'){
+                    $this->frontController->register($this->request->getPost());
+                }
+                elseif($route === 'login'){
+                    $this->frontController->login($this->request->getPost());
+                }
+                elseif($route === 'profile'){
+                    $this->backController->profile();
+                }
+                elseif($route === 'updatePassword'){
+                    $this->backController->updatePassword($this->request->getPost());
+                }
+                elseif($route === 'logout'){
+                    $this->backController->logout();
+                }
                 else{
                     $this->errorController->errorNotFound();
                 }
