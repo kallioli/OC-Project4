3ddbc6e9f480656bec4185de7e78cf27afd6463b
Kevin Allioli <kevin@linit.io>
2020-09-30 10:19:19 +0200

    Add error message for comments and other fix after evaluation

--- src/DAO/ArticleDAO.php
+++ src/DAO/ArticleDAO.php
@@ -59,6 +59,8 @@ class ArticleDAO extends DAO
 
     public function deleteArticle($articleId)
     {
+        $sql = 'DELETE FROM comment WHERE article_id = ?';
+        $this->createQuery($sql, [$articleId]);
         $sql = 'DELETE FROM article WHERE id = ?';
         $this->createQuery($sql, [$articleId]);
     }
